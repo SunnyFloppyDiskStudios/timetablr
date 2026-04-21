c7558a300701413f21d48ea5b3aa2d8874ac4fe5
SunnyFlops <arav.prasad@outlook.com>
2026-04-22 10:16:32 +1200

    Initial Commit

---
+++ timetablr/timetablrApp.swift
@@ -0,0 +1,17 @@
+//
+//  timetablrApp.swift
+//  timetablr
+//
+//  Created on 22/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct timetablrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
