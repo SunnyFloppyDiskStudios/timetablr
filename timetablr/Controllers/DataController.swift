80b39cadcc6635e363c7dd3b163ed605cf9fb998
SunnyFlops <arav.prasad@outlook.com>
2026-04-23 20:26:31 +1200

    time work begin

--- timetablr/Controllers/DataController.swift
+++ timetablr/Controllers/DataController.swift
@@ -23,21 +23,22 @@ public var userSetSubjects = [[String]]()
 
 // default structure that days will follow
 // ["period", "class or break", "start time", "end time"]
-//public var userBaseStructure = [[String]]()
+
+//public var userBaseStructure = [[Any]]()
 
 public var userBaseStructure : [[String]] = [
-    ["1", "class", "", ""],
-    ["2", "class", "", ""],
-    ["0", "break", "", ""],
-    ["3", "class", "", ""],
-    ["4", "class", "", ""],
-    ["0", "break", "", ""],
-    ["5", "class", "", ""],
+    ["1", "class", "2000-01-01 08:45:00 +0000", "2000-01-01 09:45:00 +0000"],
+    ["2", "class", "2000-01-01 09:45:00 +0000", "2000-01-01 10:45:00 +0000"],
+    ["0", "break", "2000-01-01 10:45:00 +0000", "2000-01-01 11:15:00 +0000"],
+    ["3", "class", "2000-01-01 11:15:00 +0000", "2000-01-01 12:15:00 +0000"],
+    ["4", "class", "2000-01-01 12:15:00 +0000", "2000-01-01 13:15:00 +0000"],
+    ["0", "break", "2000-01-01 13:15:00 +0000", "2000-01-01 14:00:00 +0000"],
+    ["5", "class", "2000-01-01 14:30:00 +0000", "2000-01-01 15:30:00 +0000"],
 ]
 
 
 // all the classes that have overrides
-// ["day", "period", "class or break", "start time", "end time"]]
+// ["day", "period", "class or break", "start time", "end time"]
 public var userOverrideStructures = [[String]]()
 
 
