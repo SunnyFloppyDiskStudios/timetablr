126594211798a0ef8a3bcad6973cfefebde447cc
SunnyFlops <arav.prasad@outlook.com>
2026-09-20 20:33:50 +1200

    add the ability to remove classes from timetable

--- timetablr/Views/ContentView.swift
+++ timetablr/Views/ContentView.swift
@@ -126,6 +126,17 @@ struct ContentView: View {
                             // stack containing the user's draggable set classes
                             if $state.tTableEditMode.wrappedValue {
                                 ScrollView {
+                                    Button {
+                                        selectedClass = Subject.init(name: "", colour: .secondaryBackground)
+                                    } label: {
+                                        Text("remove")
+                                            .padding()
+                                            .tint(.white)
+                                            .background(.secondaryBackground)
+                                            .cornerRadius(10)
+                                            .fontWeight(selectedClass?.name == "remove" ? .heavy : .regular)
+                                    }
+                                    
                                     LazyVGrid(columns: [GridItem(.adaptive(minimum: 100))], spacing: 10) {
                                         ForEach($data.userSubjects) { s in
                                             Button {
