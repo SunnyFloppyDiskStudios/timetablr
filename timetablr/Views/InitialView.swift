4212786aedea07866a27480d22d854e29e3d10e7
SunnyFlops <arav.prasad@outlook.com>
2026-09-21 09:00:05 +1200

    improve initial view design

--- timetablr/Views/InitialView.swift
+++ timetablr/Views/InitialView.swift
@@ -21,18 +21,36 @@ struct InitialView: View {
             ContentView(data: data, state: state)
         } else {
             VStack {
+                Spacer()
+                
                 Text("Timetablr")
-                    .font(.custom("Rubik", size: 30))
-                    .fontWeight(.bold)
+                    .font(.custom("Rubik", size: 60))
+                    .fontWeight(.heavy)
+                
+                Spacer()
+                
+                Text("Your new favourite timetabling app.")
+                    .padding(.horizontal)
+                
+                Text("Click below to get set up!")
+                    .padding(.horizontal)
+                    .padding(.bottom)
+                    .bold()
                 
                 Button {
                     transition = true
                 } label: {
                     Text("Begin Setup")
                         .font(.custom("Inter", size: 20))
+                        .tint(.white)
+                        .bold()
                 }
                 .padding()
+                .clipShape(.capsule)
+                .background(.blue)
+                .cornerRadius(10)
                 .fullScreenCover(isPresented: $transition) {
+                    // fully hides initial view and prevents going backward
                     StructureSetupView(data: data, colour: colour, state: state) // stage 1
                 }
             }
