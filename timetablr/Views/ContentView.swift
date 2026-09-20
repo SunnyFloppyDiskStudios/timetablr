c709ccce487a20813926431134814aa8455befd6
SunnyFlops <arav.prasad@outlook.com>
2026-09-20 20:03:35 +1200

    minor change to some debugging data

--- timetablr/Views/ContentView.swift
+++ timetablr/Views/ContentView.swift
@@ -160,11 +160,11 @@ struct ContentView: View {
         
     ContentView(data: data, state: StateController())
         .onAppear() {            
-            data.userSubjects.append(Subject.init(name: "mathss", colour: Color.red))
-            data.userSubjects.append(Subject.init(name: "englsih", colour: Color.blue))
-            data.userSubjects.append(Subject.init(name: "phsycis", colour: Color.green))
-            data.userSubjects.append(Subject.init(name: "digitech", colour: Color.yellow))
-            data.userSubjects.append(Subject.init(name: "art", colour: Color.purple))
+            data.userSubjects.append(Subject.init(name: "Maths", colour: Color.red))
+            data.userSubjects.append(Subject.init(name: "English", colour: Color.blue))
+            data.userSubjects.append(Subject.init(name: "Phsyics", colour: Color.green))
+            data.userSubjects.append(Subject.init(name: "DigiTech", colour: Color.yellow))
+            data.userSubjects.append(Subject.init(name: "Art", colour: Color.purple))
 
             data.userBaseDayStructure.append(Period.init(isPeriod: true, startTime: Time(hours: 8, minutes: 45), endTime: Time(hours: 9, minutes: 45)))
             data.userBaseDayStructure.append(Period.init(isPeriod: true, startTime: Time(hours: 9, minutes: 45), endTime: Time(hours: 10, minutes: 45)))
@@ -175,11 +175,11 @@ struct ContentView: View {
             data.userBaseDayStructure.append(Period.init(isPeriod: true, startTime: Time(hours: 14, minutes: 30), endTime: Time(hours: 15, minutes: 30)))
 
             data.userDaySubjects.append(DaySubjects(cycle: 1, day: 0, subjects: [
-                Class(periodID: data.userBaseDayStructure[0].id, subject: Subject(name: "Test1", colour: .red)),
-                Class(periodID: data.userBaseDayStructure[1].id, subject: Subject(name: "Test2", colour: .blue)),
-                Class(periodID: data.userBaseDayStructure[3].id, subject: Subject(name: "Test3", colour: .green)),
-                Class(periodID: data.userBaseDayStructure[4].id, subject: Subject(name: "Test4", colour: .red)),
-                Class(periodID: data.userBaseDayStructure[6].id, subject: Subject(name: "Test5", colour: .yellow))
+                Class(periodID: data.userBaseDayStructure[0].id, subject: Subject(name: "Maths", colour: .red)),
+                Class(periodID: data.userBaseDayStructure[1].id, subject: Subject(name: "English", colour: .blue)),
+                Class(periodID: data.userBaseDayStructure[3].id, subject: Subject(name: "Physics", colour: .green)),
+                Class(periodID: data.userBaseDayStructure[4].id, subject: Subject(name: "DigiTech", colour: .yellow)),
+                Class(periodID: data.userBaseDayStructure[6].id, subject: Subject(name: "Art", colour: .purple))
             ]))
         }
 }
